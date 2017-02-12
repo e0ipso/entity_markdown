d7fada39a6d975478fe5fce88ca25d359d795235
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-02-12 19:49:37 +0100

    Add a new normalizer service
    A normalizer service is a service that is tagged with the normalizer
    tag. Drupal contains a pattern to collect all services tagged with
    a particular tag and sort them by the tag priority.

---
+++ src/Normalizer/ContentEntityNormalizer.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace Drupal\entity_markdown\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer;
+
+class ContentEntityNormalizer extends SerializerAwareNormalizer implements NormalizerInterface {}
