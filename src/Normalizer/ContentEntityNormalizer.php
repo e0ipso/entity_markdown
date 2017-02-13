8c6e99b9fedf8bcf1d1bcb9ed17a6c29624ced44
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-02-13 05:45:07 +0100

    Declare the elegibility conditions for the content entity normalizer
    This normalizer should only be considered when the serializer is
    trying to normalize a content entity and using the 'markdown' format
    for that.

--- src/Normalizer/ContentEntityNormalizer.php
+++ src/Normalizer/ContentEntityNormalizer.php
@@ -3,7 +3,30 @@
 
 namespace Drupal\entity_markdown\Normalizer;
 
+use Drupal\Core\Entity\ContentEntityInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\scalar;
 use Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer;
 
-class ContentEntityNormalizer extends SerializerAwareNormalizer implements NormalizerInterface {}
+class ContentEntityNormalizer extends SerializerAwareNormalizer implements NormalizerInterface {
+
+  const FORMAT = 'markdown';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($object, $format = NULL, array $context = array()) {
+    throw new \RuntimeException('Normalization is not yet implemented.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function supportsNormalization($data, $format = NULL) {
+    // Only consider this normalizer if we are trying to normalize a content
+    // entity into the 'markdown' format.
+    return $format === static::FORMAT && $data instanceof ContentEntityInterface;
+  }
+
+
+}
