273a73582870fdb9d05122903bf6f27ec274dff0
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-02-13 06:50:25 +0100

    Add a very blunt attempt to normalize the content entities
    This commit contains an approach that will normalize all the entity
    inside of the entity normalizer. What we want to do next is to have
    different normalizers to take care of each sub object. For instance
    we want to have separate normalizes to normalize a field item list
    and another separated one to normalize a field item, and the another
    one to normalize the property.

--- src/Normalizer/ContentEntityNormalizer.php
+++ src/Normalizer/ContentEntityNormalizer.php
@@ -4,11 +4,14 @@
 namespace Drupal\entity_markdown\Normalizer;
 
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\scalar;
 use Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer;
 
 class ContentEntityNormalizer extends SerializerAwareNormalizer implements NormalizerInterface {
+
+  use StringTranslationTrait;
 
   const FORMAT = 'markdown';
 
@@ -16,7 +19,74 @@ class ContentEntityNormalizer extends SerializerAwareNormalizer implements Norma
    * {@inheritdoc}
    */
   public function normalize($object, $format = NULL, array $context = array()) {
-    throw new \RuntimeException('Normalization is not yet implemented.');
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $object */
+    // Add a heading with the label information with a default value.
+    $normalized['label'] = [
+      'prefix' => '# ',
+      'value' => $object->label() ?: $this->t('- No label found -'),
+    ];
+
+    // Add some interesting metadata.
+    $normalized = array_merge($normalized, [
+      'entity_type' => ['prefix' => '## ', 'value' => $object->getEntityTypeId()],
+      'bundle' => ['prefix' => '## ', 'value' => $object->bundle() ?: $this->t('- Entity without a bundle -')],
+      'language' => ['prefix' => '## ', 'value' => $object->language()->getName()],
+    ]);
+
+    // Add the information about the fields. This code will return an array with
+    // the information for each field in the entity.
+    $normalized_fields = array_map(function (FieldItemListInterface $field_item_list) {
+      // Now transform the field into a string version of it. We want to output
+      // the name of the field and the value.
+      $normalized_field = [
+        'name' => ['prefix' => '### ', 'value' => $field_item_list->getName()]
+      ];
+      // If the field can hold multiple values we want them as a list. If not,
+      // as a plain value. For that we need to check how the field is defined to
+      // see the cardinality value.
+      $cardinality = $field_item_list
+        ->getFieldDefinition()
+        ->getFieldStorageDefinition()
+        ->getCardinality();
+
+      // We also want to check if the field has only one property (typically
+      // called 'value'). If that is the case, we will just return the content,
+      // and not bother with the property name.
+      $field_values = $field_item_list->getValue();
+      $property_definitions = $field_item_list
+        ->getItemDefinition()
+        ->getPropertyDefinitions();
+      $num_properties = count($property_definitions);
+
+      // Normalize the array of field item values. Each one can contain multiple
+      // properties, we we'll need to normalize that as well.
+      $field_item_values = array_map(function ($field_item_value) use ($num_properties) {
+        return $this->normalizeFieldItemValue($field_item_value, $num_properties == 1);
+      }, $field_values);
+      // If this is a single field, just consider the first item.
+      if ($cardinality == 1) {
+        $normalized_field['content'] = ['value' => $field_item_values[0]];
+      }
+      else {
+        $content = array_reduce(
+          $field_item_values,
+          function ($carry, $field_item_value) {
+            // Prefix each value with a '* ' and suffix with a \n.
+            return $carry . "* $field_item_value\n";
+          },
+          "\n"
+        );
+        $normalized_field['content'] = ['value' => $content];
+      }
+
+      return $normalized_field;
+    }, $object->getFields(TRUE));
+
+    // Append the field information to the normalized data array.
+    $normalized['fields'] = ['prefix' => '## ', 'value' => $this->t('Fields')];
+    $normalized = array_merge($normalized, $normalized_fields);
+
+    return $normalized;
   }
 
   /**
@@ -28,5 +98,31 @@ class ContentEntityNormalizer extends SerializerAwareNormalizer implements Norma
     return $format === static::FORMAT && $data instanceof ContentEntityInterface;
   }
 
+  /**
+   * Normalizes a field item value.
+   *
+   * @param array $item_values
+   *   The array of values keyed by property name.
+   * @param bool $is_single_property
+   *   TRUE if this field only accepts a single value.
+   *
+   * @return string
+   *   The normalized value.
+   */
+  protected function normalizeFieldItemValue($item_values, $is_single_property) {
+    // If there is only one property, just output that one.
+    if ($is_single_property) {
+      return array_shift($item_values) ?: '';
+    }
+
+    $output = '';
+    foreach ($item_values as $property => $item_value) {
+      // This will generate a nested list (in case the field is multivalue) with
+      // the property in bold followed by a ':' and the value.
+      $output .= "    * **$property**: $item_value";
+    }
+
+    return $output;
+  }
 
 }
